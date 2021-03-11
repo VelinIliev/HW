17d6bdd33b7a257cec5e40f9dcd460661838b1ed
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2021-03-11 10:43:56 +0200

    HW2

---
+++ 04_createCatConstructor.js
@@ -0,0 +1,40 @@
+// Animal constructor
+let Animal = function(name, food){
+    this.name = name;
+    this.food = food;
+}
+Animal.prototype.eat = function(){
+    console.log(`${this.name} is eating ${this.food}`);
+}
+
+// Create a Cat Constructor, which will inherit from Animal:
+// <<<<< your code start here
+
+
+let Cat = function(name, food, sound) {
+    this.__proto__ = new Animal(name, food);
+    this.sound = sound;
+};
+
+Cat.prototype = Object.create(Animal.prototype);
+
+// Тук явно съм замзал нещо, защото не работи
+Cat.prototype.myao = function() { 
+    console.log(`${this.name} ${this.sound}`);
+}
+// >>>>> your code ends here
+
+// create tom object:
+let tom = new Cat('Tom', 'cheese', 'myao');
+let pisan = new Cat('Pisan', 'fish', 'myaooo');
+
+tom.eat();
+pisan.eat();
+
+tom.myao();
+pisan.myao();
+
+
+
+// expected output:
+// Tom is eating cheese
